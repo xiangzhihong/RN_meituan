848f9266a908ec19bd07bba9334dc234726b5c03
zhihong <xiangzhihong@users.noreply.github.com>
2018-05-28 21:27:02 +0800

    Update Navigation.js

--- app/Navigation.js
+++ app/Navigation.js
@@ -1,15 +1,11 @@
 /**
- * 路由界面
+ * 路由配置
  */
 
 import React, { Component } from 'react'
 import {View, StatusBar, Platform } from 'react-native'
 import {Navigator } from 'react-native-deprecated-custom-components'
-<<<<<<< HEAD
 import PropTypes from 'prop-types';
-=======
-import MainScreen from './main/MainScreen'
->>>>>>> 9f65954d6b618105b8e9c19ffa5f2ad8ceb1f7de
 import SplashView from './navigation/SplashView'
 
 export default class Navigation extends Component{
